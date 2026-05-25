1ecb47920bb87aa665307267ac9811485b9b3459
Syifa Ali Nurhikam <268196785+packet-ali@users.noreply.github.com>
2026-05-25 19:53:56 +0700

    Refactor main.jsx to simplify imports and structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/global.css'
 
-import {
-  BrowserRouter,
-} from "react-router-dom";
-
-import App from "./App";
-
-import "./styles/global.css";
-
-import "leaflet/dist/leaflet.css";
-
-import {
-  AuthProvider,
-} from "./context/AuthContext";
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    <BrowserRouter>
-
-      <AuthProvider>
-
-        <App />
-
-      </AuthProvider>
-
-    </BrowserRouter>
-
+    <App />
   </React.StrictMode>
-);
+)
