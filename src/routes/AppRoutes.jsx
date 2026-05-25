d403b76ca9f8100eb8d0cd906c65c7de58510930
Syifa Ali Nurhikam <268196785+packet-ali@users.noreply.github.com>
2026-05-25 19:58:26 +0700

    Refactor AppRoutes to use BrowserRouter

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,78 +1,22 @@
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-
-import Login from "../pages/auth/Login";
-
-import Dashboard from "../pages/dashboard/Dashboard";
-
-import Profile from "../pages/profile/Profile";
-
-import SetorSampah from "../pages/setor/SetorSampah";
-
-import BankSampah from "../pages/bank/BankSampah";
-
-import Transaksi from "../pages/transaksi/Transaksi";
-
-import ProtectedRoute from "./ProtectedRoute";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from '../pages/auth/Login'
+import Dashboard from '../pages/dashboard/Dashboard'
+import Profile from '../pages/profile/Profile'
+import SetorSampah from '../pages/setor/SetorSampah'
+import BankSampah from '../pages/bank/BankSampah'
+import Transaksi from '../pages/transaksi/Transaksi'
 
 export default function AppRoutes() {
-
   return (
-
-    <Routes>
-
-      <Route
-        path="/"
-        element={<Login />}
-      />
-
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/profile"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/setor"
-        element={
-          <ProtectedRoute>
-            <SetorSampah />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/bank-sampah"
-        element={
-          <ProtectedRoute>
-            <BankSampah />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/transaksi"
-        element={
-          <ProtectedRoute>
-            <Transaksi />
-          </ProtectedRoute>
-        }
-      />
-
-    </Routes>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/setor' element={<SetorSampah />} />
+        <Route path='/bank' element={<BankSampah />} />
+        <Route path='/transaksi' element={<Transaksi />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
