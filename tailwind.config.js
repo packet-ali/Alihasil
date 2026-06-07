9a197153a456ff1fde2ed8e1cec9582fdad5b416
Syifa Ali Nurhikam <268196785+packet-ali@users.noreply.github.com>
2026-06-07 10:40:51 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,42 @@
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+
+        primary: "#16A34A",
+
+        secondary: "#22C55E",
+
+        earth: "#A16207",
+
+        soft: "#F6FFF7",
+
+        darkgreen: "#14532D",
+      },
+
+      borderRadius: {
+
+        card: "24px",
+
+        section: "32px",
+      },
+
+      boxShadow: {
+
+        card:
+          "0 4px 15px rgba(0,0,0,.06)",
+
+        soft:
+          "0 10px 25px rgba(0,0,0,.08)",
+      },
+    },
   },
+
   plugins: [],
-}
+};
