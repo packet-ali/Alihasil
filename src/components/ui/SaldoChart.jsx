52d3c249cf266238028be25f22437f827b9ad63d
Syifa Ali Nurhikam <268196785+packet-ali@users.noreply.github.com>
2026-06-07 09:31:18 +0700

    Update SaldoChart.jsx

--- src/components/ui/SaldoChart.jsx
+++ src/components/ui/SaldoChart.jsx
@@ -18,12 +18,44 @@ export default function SaldoChart() {
     <div className='bg-white p-4 rounded-3xl shadow'>
       <h2 className='font-semibold mb-4'>Grafik Saldo</h2>
 
-      <ResponsiveContainer width='100%' height={250}>
+      <ResponsiveContainer width='100%' height={350}>
         <LineChart data={data}>
           <XAxis dataKey='name' />
-          <YAxis />
-          <Tooltip />
-          <Line type='monotone' dataKey='saldo' stroke='#16a34a' />
+
+          <YAxis
+            domain={[0, 100000]}
+            ticks={[
+              0,
+              10000,
+              20000,
+              30000,
+              40000,
+              50000,
+              60000,
+              70000,
+              80000,
+              90000,
+              100000,
+            ]}
+            tickFormatter={(value) =>
+              value === 0
+                ? '0'
+                : new Intl.NumberFormat('id-ID').format(value)
+            }
+          />
+
+          <Tooltip
+            formatter={(value) =>
+              `Rp ${new Intl.NumberFormat('id-ID').format(value)}`
+            }
+          />
+
+          <Line
+            type='monotone'
+            dataKey='saldo'
+            stroke='#16a34a'
+            strokeWidth={3}
+          />
         </LineChart>
       </ResponsiveContainer>
     </div>
